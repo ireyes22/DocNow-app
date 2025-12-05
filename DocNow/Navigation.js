ae59c773b9883b31ee485283cc455992ca6166da
yayis22 <159938778+ireyes22@users.noreply.github.com>
2025-12-05 15:53:07 -0700

    Creacion pantalla Home

--- DocNow/Navigation.js
+++ DocNow/Navigation.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons';
 
 
 // screens
-import home from "./screens/Home";
+import Home from "./screens/Home";
 import Appointment from "./screens/Appointment";
 import Notifications from "./screens/Notifications";
-import Porfile from "./screens/Porfile";
+import Profile from "./screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
+const PrimaryColor = '#0A3B74';
+const SecondaryColor = '#498FC0';
   
 function MyTabs() {
   return (
@@ -18,14 +21,46 @@ function MyTabs() {
       initialRouteName="Home"
       screenOptions={{ 
         headerShown: false,
-        tabBarInactiveTintColor: '#0A3B74',
-        tabBarActiveTintColor: '#071e39ff',
+        tabBarInactiveTintColor: PrimaryColor,
+        tabBarActiveTintColor: SecondaryColor,
       }}
     >
-      <Tab.Screen name="Home" component={home} />
-      <Tab.Screen name="Citas" component={Appointment} />
-      <Tab.Screen name="Notificaciones" component={Notifications} />
-      <Tab.Screen name="Perfil" component={Porfile} />
+      <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Citas" 
+        component={Appointment} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Notificaciones" 
+        component={Notifications} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="notifications" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Perfil" 
+        component={Profile} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- DocNow/screens/Profile.js
+++ DocNow/screens/Profile.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Porfile = () => {
+const Profile = () => {
   return (
     <View style={styles.container}>
 
@@ -20,4 +20,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Porfile;
+export default Profile;
